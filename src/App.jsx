283015fd07600dda01f411b0032f5f8819b2a4a3
Mutonga Romeo <romeomutonga3@gmail.com>
2025-01-09 12:10:18 +0300

    Updated

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,6 @@ import Contact from './components/contact/Contact'
 import Experience from './components/experience/Experience'
 import Header from './components/header/Header'
 import Footer from './components/footer/Footer'
-import Nav from './components/nav/Nav'
 import Portfolio from './components/portfolio/Portfolio'
 import Services from './components/services/Services'
 import Testimonials from './components/testimonials/Testimonials'
@@ -16,7 +15,6 @@ const App = () => {
     return (
         <>
             <Header />
-            <Nav />
             <About />
             <Experience />
             <Services />
